b1198459774fd6c287bac2d89b1adedeb45dc91a
rishu <rock.jade09@gmail.com>
2024-07-01 10:55:25 +0530

    first commit

---
+++ js/authentication.js
@@ -0,0 +1,50 @@
+// authentication 
+
+
+document.addEventListener('DOMContentLoaded', () => {
+
+    const token = localStorage.getItem('token');
+    const employertoken = localStorage.getItem('employertoken');
+     
+    const authButton = document.getElementById('auth-button');
+    const authLink = document.getElementById('auth-link');
+    const authBtn = document.getElementById('auth-btn');
+
+    const innertext = document.getElementById('employertoken');
+  
+    if (token ) {
+      // User is logged in, show "Log out"
+      authLink.textContent = 'Log out';
+      authLink.removeAttribute('data-toggle');
+      authLink.removeAttribute('data-target');
+      authLink.href = '#';
+      authBtn.classList.remove('btn-danger');
+      authBtn.classList.add('btn-success');
+  
+
+
+      authButton.addEventListener('click', () => {
+        // Handle log out
+        localStorage.removeItem('token');
+        window.location.reload();
+      });
+    } else {
+      // User is not logged in, show "Sign in"
+      authLink.textContent = 'Sign in';
+      authLink.setAttribute('data-toggle', 'modal');
+      authLink.setAttribute('data-target', '#exampleModal');
+      authLink.href = '#';
+      authBtn.classList.remove('btn-success');
+      authBtn.classList.add('btn-danger');
+    }
+
+    if( employertoken)  {
+      innertext.textContent = localStorage.getItem('employername')
+         
+    }
+    else {
+      innertext.textContent = 'employer login'
+     
+    }
+
+  });
---
+++ js/main.js
@@ -0,0 +1,297 @@
+ /*$("#editModal").on('show.bs.modal', function (e) {
+    $("#templateModal").modal("hide");
+});*/
+
+$(document).ready(function () {
+  //can also be wrapped with:
+  //1. $(function () {...});
+  //2. $(window).load(function () {...});
+  //3. Or your own custom named function block.
+  //It's better to wrap it.
+
+  //Tooltip, activated by hover event
+  $("body").tooltip({   
+    selector: "[data-toggle='tooltip']",
+    container: "body"
+  })
+    //Popover, activated by clicking
+    .popover({
+    selector: "[data-toggle='popover']",
+    container: "body",
+    html: true
+  });
+  //They can be chained like the example above (when using the same selector).
+  
+});
+/*-----------------------------*/
+
+
+
+
+$('#dropdown').hover(function() {
+  $(this).find('.dropdown-menu').stop(true, true).delay(200).fadeIn(500);
+}, function() {
+  $(this).find('.dropdown-menu').stop(true, true).delay(200).fadeOut(500);
+});
+/*--------------------------------------*/
+$(document).ready(function() {
+$(".scroll").click(function(event) {
+$('html,body').animate({ scrollTop: $(this.hash).offset().top }, 500);
+});
+});
+/*-----------------------*/
+
+/*-------------------*/
+
+$(document).ready(function() {
+  prep_modal();
+});
+
+function prep_modal()
+{
+  $(".modal").each(function() {
+
+  var element = this;
+  var pages = $(this).find('.modal-split');
+
+  if (pages.length != 0)
+  {
+      pages.hide();
+      pages.eq(0).show();
+
+      var b_button = document.createElement("button");
+                b_button.setAttribute("type","button");
+                b_button.setAttribute("class","btn btn-success btn-sm");
+                b_button.setAttribute("style","display: none;");
+                b_button.innerHTML = "Back";
+
+      var n_button = document.createElement("button");
+                n_button.setAttribute("type","button");
+                n_button.setAttribute("class","btn btn-warning btn-sm");
+                n_button.innerHTML = "Next";
+
+      $(this).find('.modal-footer').append(b_button).append(n_button);
+
+
+      var page_track = 0;
+
+      $(n_button).click(function() {
+        
+        this.blur();
+
+        if(page_track == 0)
+        {
+          $(b_button).show();
+        }
+
+        if(page_track == pages.length-2)
+        {
+          $(n_button).text("Submit");
+        }
+
+        if(page_track == pages.length-1)
+        {
+          $(element).find("form").submit();
+        }
+
+        if(page_track < pages.length-1)
+        {
+          page_track++;
+
+          pages.hide();
+          pages.eq(page_track).show();
+        }
+
+
+      });
+
+      $(b_button).click(function() {
+
+        if(page_track == 1)
+        {
+          $(b_button).hide();
+        }
+
+        if(page_track == pages.length-1)
+        {
+          $(n_button).text("Next");
+        }
+
+        if(page_track > 0)
+        {
+          page_track--;
+
+          pages.hide();
+          pages.eq(page_track).show();
+        }
+
+
+      });
+
+  }
+
+  });
+}
+
+
+
+/*toggle*/
+
+/*-----------------------------------*/
+
+(function($) {
+    "use strict";
+
+    // manual carousel controls
+    $('.next').click(function(){ $('.carousel').carousel('next');return false; });
+    $('.prev').click(function(){ $('.carousel').carousel('prev');return false; });
+    
+})(jQuery);
+
+
+/*-----------------------------------------------------------*/
+
+
+
+
+/*range slider*/
+/*$(document).ready(function() {
+    $('body').bootstrapMaterialDesign();});*/
+
+
+$(document).ready(function(){
+    $('a[data-toggle="tab"]').on('show.bs.tab', function(e) {
+        localStorage.setItem('activeTab', $(e.target).attr('href'));
+    });
+    var activeTab = localStorage.getItem('activeTab');
+    if(activeTab){
+        $('#myTab a[href="' + activeTab + '"]').tab('show');
+    }
+});
+
+/*---------------------------------*/
+
+    $(document).ready(function(){
+        // Add down arrow icon for collapse element which is open by default
+        $(".collapse.show").each(function(){
+            $(this).prev(".card-header").find(".fa").addClass("fa-angle-down").removeClass("fa-angle-right");
+        });
+        
+        // Toggle right and down arrow icon on show hide of collapse element
+        $(".collapse").on('show.bs.collapse', function(){
+            $(this).prev(".card-header").find(".fa").removeClass("fa-angle-right").addClass("fa-angle-down");
+        }).on('hide.bs.collapse', function(){
+            $(this).prev(".card-header").find(".fa").removeClass("fa-angle-down").addClass("fa-angle-right");
+        });
+    });
+
+
+
+    /*cart js*/
+$(document).ready(function(){
+  $('.toggle').click(function(){
+    $('.sidebar-contact').toggleClass('active')
+    $('.toggle').toggleClass('active')
+  })
+})
+
+
+
+
+
+
+
+
+
+
+
+
+/* Set rates + misc */
+var taxRate = 0.05;
+var shippingRate = 15.00; 
+var fadeTime = 300;
+
+
+/* Assign actions */
+$('.product-quantity input').change( function() {
+  updateQuantity(this);
+});
+
+$('.product-removal button').click( function() {
+  removeItem(this);
+});
+
+
+/* Recalculate cart */
+function recalculateCart()
+{
+  var subtotal = 0;
+  
+  /* Sum up row totals */
+  $('.product').each(function () {
+    subtotal += parseFloat($(this).children('.product-line-price').text());
+  });
+  
+  /* Calculate totals */
+  var tax = subtotal * taxRate;
+  var shipping = (subtotal > 0 ? shippingRate : 0);
+  var total = subtotal + tax + shipping;
+  
+  /* Update totals display */
+  $('.totals-value').fadeOut(fadeTime, function() {
+    $('#cart-subtotal').html(subtotal.toFixed(2));
+    $('#cart-tax').html(tax.toFixed(2));
+    $('#cart-shipping').html(shipping.toFixed(2));
+    $('#cart-total').html(total.toFixed(2));
+    if(total == 0){
+      $('.checkout').fadeOut(fadeTime);
+    }else{
+      $('.checkout').fadeIn(fadeTime);
+    }
+    $('.totals-value').fadeIn(fadeTime);
+  });
+}
+
+
+/* Update quantity */
+function updateQuantity(quantityInput)
+{
+  /* Calculate line price */
+  var productRow = $(quantityInput).parent().parent();
+  var price = parseFloat(productRow.children('.product-price').text());
+  var quantity = $(quantityInput).val();
+  var linePrice = price * quantity;
+  
+  /* Update line price display and recalc cart totals */
+  productRow.children('.product-line-price').each(function () {
+    $(this).fadeOut(fadeTime, function() {
+      $(this).text(linePrice.toFixed(2));
+      recalculateCart();
+      $(this).fadeIn(fadeTime);
+    });
+  });  
+}
+
+/*upload*/
+function readURL(input) {
+  if (input.files && input.files[0]) {
+
+    var reader = new FileReader();
+
+    reader.onload = function(e) {
+      $('.image-upload-wrap').hide();
+
+      $('.file-upload-image').attr('src', e.target.result);
+      $('.file-upload-content').show();
+
+      $('.image-title').html(input.files[0].name);
+    };
+
+    reader.readAsDataURL(input.files[0]);
+
+  } else {
+    removeUpload();
+  }
+}
+
+
